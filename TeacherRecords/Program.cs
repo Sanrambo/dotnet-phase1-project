11e56bbbda1969c5c0098c1b1c4ee365a5e60651
sanrambo <gh.r4mbo@gmail.com>
2021-10-04 12:13:32 +0300

    Added List to pick from

--- TeacherRecords/Program.cs
+++ TeacherRecords/Program.cs
@@ -27,7 +27,7 @@ namespace TeacherRecords
 
                 //Split it by comma
                 string[] split = row.Split(',');
-
+                
                 //Define the Student class
                 Student newStudents = new Student();
 
@@ -41,66 +41,167 @@ namespace TeacherRecords
                 {
                     Console.WriteLine("Something wrong, please try again.");
                 }
-                
+
                 //Store it in student List
                 students.Add(newStudents);
             }
 
-            
-            //User insert data
-            Console.WriteLine("Insert ID, Name and Class of the new student:");
-            
-            Console.WriteLine("ID: ");
-            string insertedId = Console.ReadLine();
-            Console.WriteLine("Name: ");
-            string insertedName = Console.ReadLine();
-            Console.WriteLine("Class: ");
-            string insertedClass = Console.ReadLine();
-
-            //Store inserted data into student List
-            if (insertedId != "" && insertedClass != "" && insertedName != "" ) {
-                Student studentAssign = new Student
-                {
-                    Id = insertedId,
-                    Name = insertedName,
-                    Class = insertedClass
-                };
+            Console.WriteLine("Press the needed option\n 1- Add student\n 2- List students\n 3- Search for student\n 4- Remove student");
+            var c = Console.ReadLine();
 
 
-                students.Add(studentAssign);
-          
+            switch (c) {
+                case "1":
+                    // public void Inserted(int id, string Name, string Class) {
+                    //User insert data
+                    Console.WriteLine("Insert ID, Name and Class of the new student:");
 
-            //Define data List
-            List<string> data = new List<string>();
+                    Console.WriteLine("ID: ");
+                    string insertedId = Console.ReadLine();
+                    Console.WriteLine("Name: ");
+                    string insertedName = Console.ReadLine();
+                    Console.WriteLine("Class: ");
+                    string insertedClass = Console.ReadLine();
 
-            //Go through student List and store the inserted data into "data" List
-            foreach(var student in students)
-            {
-                data.Add($"{student.Id},{student.Name},{student.Class}");
+
+                    //Store inserted data into student List
+                    if (insertedId != "" && insertedClass != "" && insertedName != "") {
+                        Student studentAssign = new Student
+                        {
+                            Id = insertedId,
+                            Name = insertedName,
+                            Class = insertedClass
+                        };
+
+
+                        students.Add(studentAssign);
+
+
+                        //Define data List
+                        List<string> data = new List<string>();
+
+                        //Go through student List and store the inserted data into "data" List
+                        foreach (var student in students)
+                        {
+                            data.Add($"{student.Id},{student.Name},{student.Class}");
+                        }
+
+                        //Write only the inserted data which is store in "data" List into the text file
+
+                        File.WriteAllLines(@"records.txt", data);
+
+
+                        Console.WriteLine("Data has been inserted");
+
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter proper input");
+                    }
+
+                    break;
+                case "2":
+
+
+                    //Print the list from txt file to check if the data i` s inserted
+                    Console.WriteLine("----- Print the data: ");
+                    foreach (var student in students)
+                    {
+                        //Printing the output in a specific form seperated by |
+                        Console.WriteLine($"{student.Id} | {student.Name} | {student.Class}");
+                    }
+
+                    break;
+                case "3":
+                    Console.WriteLine("Choose the way to find the student\n 1- By ID\n 2- By Name");
+                    var optionId = Console.ReadLine();
+
+                    switch (optionId) {
+
+                    case "1":
+                    Console.WriteLine("Please enter student ID:");
+                    var gotId = Console.ReadLine();
+
+                    foreach (var studentId in students)
+                    {
+                        if (gotId == studentId.Id)
+                        {
+                            Console.WriteLine($"{studentId.Id} | {studentId.Name} | {studentId.Class}");
+                        }
+                    }
+
+                    break;
+                    case "2":
+                    Console.WriteLine("Please enter student name:");
+                    var gotName = Console.ReadLine();
+
+                    foreach (var studentName in students)
+                    {
+                        if (gotName == studentName.Name)
+                        {
+                            Console.WriteLine($"{studentName.Id} | {studentName.Name} | {studentName.Class}");
+                        }
+                    }
+                    break;
+                        default:
+                            break;
+
             }
+                    break;
 
-            //Right only the inserted data which is store in "data" List into the text file
-            
-            File.WriteAllLines(myPath, data);
+                    case "4":
+                    Console.WriteLine("Choose the way to remove student\n 1- By ID\n 2- By Name");
+                    var removeId = Console.ReadLine();
+
+                    Console.WriteLine("Enter the new value:");
+                    var name = Console.ReadLine();
+
+                    //StreamReader reading = File.OpenText(@"records.txt");
+
+                    foreach (var studentRemoved in students)
+                    {
+                        if (removeId == studentRemoved.Id)
+                        {
+                            //Student studentRemove = new Student
+                            //{
+                            //    Id = studentRemoved.Id,
+                            //    Name = studentRemoved.Name,
+                            //    Class = studentRemoved.Class
+                            //};
+
+                            studentRemoved.Name = name;
 
 
-            Console.WriteLine("Data has been inserted");
+                            // Console.WriteLine(studentRemove);
 
-            //Print the list from txt file to check if the data is inserted
-            Console.WriteLine("----- Print the data: ");
-            foreach (var student in students)
-            {
-                //Printing the output in a specific form seperated by |
-                Console.WriteLine($"{student.Id} | {student.Name} | {student.Class}");
-            }
-                //Let user enter something or press enter to finish the program 
-            }
-            else
-            {
-                Console.WriteLine("Please enter proper input");
-            }
+                            //=students.Remove(studentRemove);
+                            //File.WriteAllLines(myPath, students);
+                            break;
 
-            Console.ReadLine();
+                            //Console.WriteLine("Removed");
+                        }
+
+                      
+
+
+                    }
+
+                    foreach (var student in students)
+                    {
+                        //Printing the output in a specific form seperated by |
+                        Console.WriteLine($"{student.Id} | {student.Name} | {student.Class}");
+                    }
+
+                    break;
+                default:
+                    break;
+
+        }
+
+        }
+        //Let user enter something or press enter to finish the program 
+        //Console.ReadLine();
                  }
     }
-}
